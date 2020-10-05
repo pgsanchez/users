148b116b9f0401420be9fa8821b8421b9135e352
pgsanchez <pedrogonzalez.sanchez@gmail.com>
2020-10-05 13:49:01 +0200

    V2.1 Gestión de usuarios con seguridad. Fichero de propiedades para acceder a la BD y password encriptada

--- src/main/java/com/pgsanchez/users/controller/HomeController.java
+++ src/main/java/com/pgsanchez/users/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.pgsanchez.users.controller;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,12 @@ public class HomeController {
 	public String welcome(Model model) {
 		model.addAttribute("greeting", "Welcome to Web Users!");
 		model.addAttribute("tagline", "The one and only amazing users web");
+		
+		//String password = "admin123";
+        //BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        //System.out.println("Encoded password is " + passwordEncoder.encode(password));
+        
+        
 		return "welcome";
 	}
 	
